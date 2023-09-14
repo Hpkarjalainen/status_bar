229fa53e13bc5e0bffd1f050c485235bff1e87de
Hannu Pekka <t1kaha00@students.oamk.fi>
2023-09-14 21:38:16 +0300

    all done

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
+import Constants from 'expo-constants'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>A few options for the statusbar</Text>
+      <Text>It is now set to have a black background and white text</Text>
+      <StatusBar 
+      style="light"
+      backgroundColor='black'
+      //hidden={true}
+       />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
   },
 });
